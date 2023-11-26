426c1c20b598bdfdaa8b2905772ad890d9a3d4b8
julianaMHR <150975809+JuliMHR@users.noreply.github.com>
2023-11-26 10:15:23 -0500

    Update primerarchivo.py

--- primerarchivo.py
+++ primerarchivo.py
@@ -1,21 +1,22 @@
-import tkinter as tk
+from tkinter import *
 
 #Características generales del jugador
 nombre_jugador = ""
 
-
 # Inicialización de la ventana usando tkinter
-principal = tk.Tk()
+principal = Tk()
 principal.title("Perdido en el Laberinto: Un Juego Simple Basado en Texto")
+#principal.iconbitmap(r"C:\Users\Juliana\Desktop\perdido en el laberinto\imagenes\lab.ico") icono
+principal.geometry("550x350")
 
 #Introducción y configuración del jugador
-etiqueta_intro = tk.Label(principal, text = "Bienvenido a Perdido en el Laberinto, ¿estás dispuesto a comenzar?\n")
+etiqueta_intro = Label(principal, text = "Bienvenido a Perdido en el Laberinto, ¿estás dispuesto a comenzar?\n")
 etiqueta_intro.pack()
 
-etiqueta_nombre = tk.Label(principal, text = "Ingresa tu nombre")
+etiqueta_nombre = Label(principal, text = "Ingresa tu nombre")
 etiqueta_nombre.pack()
 
-ingreso_nombre = tk.Entry(principal)
+ingreso_nombre = Entry(principal)
 ingreso_nombre.pack()
 
 
@@ -44,14 +45,17 @@ def comienzoHistoria():
 
 def buscarEnLaHabitación():
     boton_buscar_en_la_habitacion.destroy()
-    boton_afrontar_la_puerta.destroy()
+    #boton_afrontar_la_puerta.destroy()
     etiqueta_texto_inicial.destroy()
     
     etiqueta_texto_buscar_en_la_habitacion.pack()
     
     boton_inspeccionar_simbolos.pack()
+    boton_afrontar_la_puerta.place(x=200,y=100)
 
 def afrontarLaPuerta():
+    boton_inspeccionar_simbolos.destroy()
+    etiqueta_texto_buscar_en_la_habitacion.destroy()
     boton_buscar_en_la_habitacion.destroy()
     boton_afrontar_la_puerta.destroy()
     etiqueta_texto_inicial.destroy()
@@ -64,34 +68,94 @@ def afrontarLaPuerta():
 def cruzarElPuente():
     boton_cruzar_el_puente.destroy()
     boton_explorar_otro_camino.destroy()
-    
+    etiqueta_texto_memorizar_mapa_cruzar_puerta.destroy()
+    etiqueta_texto_afrontar_la_puerta.destroy()
+    boton_cruzar_el_puente.destroy()
+    boton_explorar_otro_camino.destroy()
+
+    etiqueta_texto_cruzar_el_puente.pack()
+    boton_explorar_otro_camino.pack()
+    boton_seguir_cruzando_el_puente.pack()
+
+def seguirCruzandoElPuente():
+    etiqueta_texto_cruzar_el_puente.distroy()
+    boton_explorar_otro_camino.distroy()
+    boton_seguir_cruzando_el_puente.distroy()
+
+    etiqueta_texto_continuar_cruzando_el_puente.pack()
+
 def explorarOtroCamino():
+    boton_cruzar_el_puente.destroy()
+    boton_explorar_otro_camino.destroy()
+    etiqueta_texto_memorizar_mapa_cruzar_puerta.destroy()
     boton_cruzar_el_puente.destroy()
     boton_explorar_otro_camino.destroy()
     
 def inspeccionarSimbolos():
     boton_inspeccionar_simbolos.destroy()
     etiqueta_texto_buscar_en_la_habitacion.destroy()
+    boton_afrontar_la_puerta.destroy()
     
     etiqueta_texto_inspeccionar_simbolos.pack()
+    boton_arrancar_simbolos.pack()
+    boton_memorizar_mapa.pack()
+
+def arrancarSimbolos():
+    etiqueta_texto_inspeccionar_simbolos.destroy()
+    boton_arrancar_simbolos.destroy()
+    boton_memorizar_mapa.destroy()
+
+    etiqueta_texto_arrancar_simbolos.pack()
+    boton_atravezar_puerta_que_se_cierra.pack()
+    boton_buscar_otra_salida.pack()
+
+
+def memorizarMapaCruzarPuerta():
+    etiqueta_texto_inspeccionar_simbolos.destroy()
+    boton_arrancar_simbolos.destroy()
+    boton_memorizar_mapa.destroy()
+
+    etiqueta_texto_memorizar_mapa_cruzar_puerta.pack()
+    boton_cruzar_el_puente.pack()
+    boton_explorar_otro_camino.pack()
+
+
+def atravesarPuertaQueSeCierra():
+    etiqueta_texto_arrancar_simbolos.destroy()
+    boton_atravezar_puerta_que_se_cierra.destroy()
+    boton_buscar_otra_salida.destroy()
+
+    etiqueta_texto_atravezar_puerta_que_se_cierra()
     
-    
-    
+
+def buscarOtraSalida50():
+    etiqueta_texto_arrancar_simbolos.destroy()
+    boton_atravezar_puerta_que_se_cierra.destroy()
+    boton_buscar_otra_salida.destroy()
+
+
+
 #Función terminar ejecución
 def salirJuego():
     principal.destroy()
 
 #Definición de botones para interacción
-boton_inicio = tk.Button(principal, text = "Comenzar", command = comienzoHistoria)
+boton_inicio = Button(principal, text = "Comenzar", command = comienzoHistoria)
 boton_inicio.pack()
-boton_salir = tk.Button(principal, text = "Salir", command = salirJuego)
-boton_buscar_en_la_habitacion = tk.Button(principal, text = "Buscar en la habitación", command = buscarEnLaHabitación)
-boton_afrontar_la_puerta = tk.Button(principal, text = "Afrontar la puerta", command = afrontarLaPuerta)
-boton_cruzar_el_puente = tk.Button(principal, text = "Cruzar el puente", command = cruzarElPuente)
-boton_explorar_otro_camino = tk.Button(principal, text = "Explorar otro camino", command = explorarOtroCamino)
-boton_inspeccionar_simbolos = tk.Button(principal, text = "Inspeccionar los Símbolos en las Paredes", command = inspeccionarSimbolos)
+boton_salir = Button(principal, text = "Salir", command = salirJuego)
+boton_buscar_en_la_habitacion = Button(principal, text = "Buscar en la habitación", command = buscarEnLaHabitación)
+boton_afrontar_la_puerta = Button(principal, text = "Afrontar la puerta", command = afrontarLaPuerta)
+boton_cruzar_el_puente = Button(principal, text = "Cruzar el puente", command = cruzarElPuente)
+boton_explorar_otro_camino = Button(principal, text = "Explorar otro camino", command = explorarOtroCamino)
+boton_inspeccionar_simbolos = Button(principal, text = "Inspeccionar los Símbolos en las Paredes", command = inspeccionarSimbolos)
+boton_arrancar_simbolos = Button(principal, text = "Arrancar los símbolos y conservarlos", command = arrancarSimbolos)
+boton_memorizar_mapa = Button(principal, text = "Memorizar el mapa y avanzar por la puerta", command = memorizarMapaCruzarPuerta)
+boton_atravezar_puerta_que_se_cierra = Button(principal, text = "Atravesar rápidamente la puerta que se cierra", command = atravesarPuertaQueSeCierra)
+boton_buscar_otra_salida = Button(principal, text ="Buscar otra salida", command = buscarOtraSalida50)
+boton_seguir_cruzando_el_puente = Button(principal, text ="Continuar cruzando el puente", command = seguirCruzandoElPuente)
 
-#Textos
+
+# Textos
 texto_inicial = (
         f"Tus ojos se abren de golpe, te encuentras en una habitación tenue con el sabor metálico del miedo en el aire.\n"
         f"El sudor frío recorre tu frente mientras te pones de pie, dándote cuenta de que estás solo.\n" 
@@ -116,14 +180,51 @@ texto_inspeccionar_simbolos = (
         f"'Cuidado con las criaturas, no confíes en nadie'\n"
         f"Armado con esta advertencia, te preparas para el viaje que tienes por delante.\n"
 )
+texto_memorizar_mapa_cruzar_puerta = (
+        f"Entras en el laberinto, entre sus giros y vueltas retumban sonidos escalofriantes.\n"
+        f"Criaturas acechan en las sombras, y te enfrentas a tu primer desafío:\n"
+        f"Un puente precario sobre un abismo aparentemente interminable.\n "
+        f"Cierras los ojos e intentas recordar el mapa que intentaste memorizar,\n"
+        f"hay algo en ese puente que te dice que debes cruzarlo\n"
+)
+texto_arrancar_simbolos = (
+        f"Con urgencia, arrancas los símbolos de las paredes, pensando que podrían ser cruciales para tu supervivencia.\n "
+        f"Sin embargo, al guardarlos apresuradamente en tus bolsillos, un ruido abominable resuena en la habitación. \n"
+        f"Las paredes parecen temblar, y la puerta de salida comienza a cerrarse.\n"
+)
+texto_atravezar_puerta_que_se_cierra =(
+        f"Corres hacia la puerta que se cierra, apenas logrando pasar antes de que se selle.\n"
+        f"La habitación se transforma en un laberinto, ves cómo la forma del laberinto se transforma frente a tus ojos \n"
+        f"de manera impredecible. Los símbolos rasgados en tus manos son ahora indescifrables.\n"
+)
+texto_buscar_otra_salida50 =(
+        f"pag 50\n"
+)
+texto_explorar_otro_camino =(
+        f"h"
+)
+texto_cruzar_el_puente =(
+        f"Te acercas con cautela al precario puente, el eco de tus pasos se mezcla con el sonido espeluznante del\n"
+         f"laberinto. Cuando subes a la primera tabla, ésta cruje siniestramente pero mantiene tu peso.\n"
+)
+texto_continuar_cruzando_el_puente =(
+        f"Con pasos cuidadosos, navegas por el puente traicionero. El abismo de abajo parece interminable,\n"
+        f"pero llegas ileso al otro lado. El laberinto se abre y te dirige al interior de una caverna que\n"
+        f"es una camara con múltiples caminos.\n"
+)
+
 
 #Etiquetas de texto
-etiqueta_texto_inicial = tk.Label(principal, text = texto_inicial)
-etiqueta_texto_buscar_en_la_habitacion = tk.Label(principal, text = texto_buscar_en_la_habitacion)
-etiqueta_texto_afrontar_la_puerta = tk.Label(principal, text = texto_afrontar_la_puerta)
-etiqueta_texto_inspeccionar_simbolos = tk.Label(principal, text = texto_inspeccionar_simbolos)
-
-
+etiqueta_texto_inicial = Label(principal, text = texto_inicial)
+etiqueta_texto_buscar_en_la_habitacion = Label(principal, text = texto_buscar_en_la_habitacion)
+etiqueta_texto_afrontar_la_puerta = Label(principal, text = texto_afrontar_la_puerta)
+etiqueta_texto_inspeccionar_simbolos = Label(principal, text = texto_inspeccionar_simbolos)
+etiqueta_texto_arrancar_simbolos = Label(principal, text = texto_arrancar_simbolos)
+etiqueta_texto_memorizar_mapa_cruzar_puerta = Label(principal, text =texto_memorizar_mapa_cruzar_puerta)
+etiqueta_texto_buscar_otra_salida50 = Label(principal, text = texto_buscar_otra_salida50)
+etiqueta_texto_atravezar_puerta_que_se_cierra = Label(principal, text = texto_atravezar_puerta_que_se_cierra)
+etiqueta_texto_cruzar_el_puente = Label(principal, text =texto_cruzar_el_puente)
+etiqueta_texto_continuar_cruzando_el_puente = Label(principal, text = texto_continuar_cruzando_el_puente)
 
 #Loop principal
 principal.mainloop()
